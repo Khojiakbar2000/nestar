94831eafb6e4a9440b3fae13b9465b911d961513
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-06-16 18:43:14 -0700

    feat: develop getMember GraphQL api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -7,6 +7,7 @@ import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/member/member.update';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class MemberService {
@@ -54,11 +55,21 @@ export class MemberService {
         if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
         result.accessToken = await this.authService.createToken(result);
+        
         return result;
        }
 
-       public async getMember(): Promise<string>{
-        return "getMember executed";
+       public async getMember(targetId: ObjectId): Promise<Member>{
+        const search: T = {
+            _id: targetId,
+            memberStatus: {
+                $in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
+
+            },
+        };
+        const targetMember = await this.memberModel.findOne(search).exec();
+        if(!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        return targetMember;
        }
 
        public async getAllMembersByAdmin(): Promise<string>{

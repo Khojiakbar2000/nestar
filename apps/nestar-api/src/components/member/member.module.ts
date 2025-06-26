eb2b0ea49751dbd34cabc5c10540900f166401d5
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-06-25 22:02:49 -0700

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -5,13 +5,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
+import { LikeModule } from '../like/like.module';
 
 @Module({
   imports: [
   MongooseModule.forFeature([{name: "Member", schema: MemberSchema}]), AuthModule,
   ViewModule,
-  MemberModule
+  MemberModule,
+  LikeModule
   ],
+
   providers: [MemberResolver, MemberService],
   exports: [MemberService]
 })

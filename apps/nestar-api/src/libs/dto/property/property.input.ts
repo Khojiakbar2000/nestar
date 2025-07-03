41f57fdc774ab48b585155b6df19ba483566aa52
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-07-03 03:33:39 -0700

    feat: develop getFavorites GraphQL api

--- apps/nestar-api/src/libs/dto/property/property.input.ts
+++ apps/nestar-api/src/libs/dto/property/property.input.ts
@@ -261,5 +261,24 @@ export class AllPropertiesInquiry{
 
 
 
+@InputType()
+export class OrdinaryInquiry{
+    @IsNotEmpty()
+    @Min(1)
+    @Field(()=> Int)
+    page: number;
+
+    @IsNotEmpty()
+    @Min(1)
+    @Field(()=> Int)
+    limit: number;
+
+  
+
+ 
+}
+
+
+
 
 

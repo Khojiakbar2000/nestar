3b2fd21f2269db35bca3ab6e714899b4128bc86a
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-08-06 00:20:14 -0700

    fix: modify Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -2,15 +2,22 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
 import * as WebSocket from "ws"
+import { AuthService } from '../components/auth/auth.service';
+import { Member } from '../libs/dto/member/member';
+import * as url from "url";
+import { Message } from '../libs/enums/common.enum';
 
 interface MessagePayload {
   event: string;
   text: string;
+  memberData: Member;
 }
 
 interface InfoPayload {
   event: string;
   totalClients: number;
+  memberData: Member;
+  action: string
 }
 
 
@@ -18,6 +25,11 @@ interface InfoPayload {
 export class SocketGateway implements OnGatewayInit  {
   private logger: Logger = new Logger("SocketEventsGateway");
   private summaryClient: number = 0;
+  private clientsAuthMap = new Map<WebSocket, Member>();
+private messagesList: MessagePayload[] = [];
+  constructor(private authService: AuthService){
+
+  }
 
   @WebSocketServer()
   server: Server;
@@ -25,35 +37,62 @@ export class SocketGateway implements OnGatewayInit  {
   public afterInit(server: Server ) {
     this.logger.verbose(`Websocket Server initialized & total: [${this.summaryClient}]`);
   }
-  handleConnection(client:WebSocket, ...args: any[] ){
-    this.summaryClient++;
-    this.logger.verbose(`Connection & total: [${this.summaryClient}]`);
 
+  private async retrieveAuth(req: any): Promise<Member>{
+try{
+  const parseUrl = url.parse(req.url, true);
+  const {token} = parseUrl.query;
+  return await this.authService.verifyToken(token as string)
+
+}catch(err){
+return null;
+}
+  }
+  public async handleConnection(client:WebSocket, req: any ){
+    const authMember = await this.retrieveAuth(req);
+    this.summaryClient++;
+   
+this.clientsAuthMap.set(client, authMember);
+const clientNick: string = authMember?.memberNick ?? "Guest";
+
+    this.logger.verbose(`Connection [${clientNick}] & total: [${this.summaryClient}]`);
+    console.log("authMember:", authMember)
     const infoMsg: InfoPayload ={
       event: 'info',
-      totalClients: this.summaryClient
+      totalClients: this.summaryClient,
+      memberData: authMember,
+      action: "joined"
     };
 this.emitMessage(infoMsg)
-    
+    client.send(JSON.stringify({event: 'getMessages', list: this.messagesList}));
 
     }
 
-    handleDisconnect(client: WebSocket){
+    public async handleDisconnect(client: WebSocket){
+      const authMember = this.clientsAuthMap.get(client);
       this.summaryClient--;
-      this.logger.verbose(`Disconnection & total: [${this.summaryClient}]`);
+      this.clientsAuthMap.delete(client);
+      const clientNick: string = authMember?.memberNick ?? "Guest";
+
+      this.logger.verbose(`Disconnection[${clientNick}] & total: [${this.summaryClient}]`);
 
       const infoMsg: InfoPayload ={
         event: 'info',
-        totalClients: this.summaryClient
+        totalClients: this.summaryClient,
+        memberData: authMember,
+        action: "left",
       };
       // client - disconnect
       this.broadcastMessage(client, infoMsg)
   }
   @SubscribeMessage('message')
   public async handleMessage(client: WebSocket, payload: string): Promise<void> {
-    const newMessage: MessagePayload = {event: "message", text: payload}
-
-  this.logger.verbose(`NEW MESSAGE: ${payload}`)
+    const authMember = this.clientsAuthMap.get(client);
+    const newMessage: MessagePayload = {event: "message", text: payload, memberData: authMember}
+    const clientNick: string = authMember?.memberNick ?? "Guest";
+  this.logger.verbose(`NEW MESSAGE [${clientNick}]: ${payload}`)
+  this.messagesList.push(newMessage);
+  if(this.messagesList.length > 5) this.messagesList.splice(0, this.messagesList.length -5);
 this.emitMessage(newMessage)
   }
 
@@ -73,3 +112,10 @@ this.server.clients.forEach((client)=> {
 })
   }
 }
+
+/*
+MESSAGE TARGET:
+1.Cleint (only client)
+2.Broadcast (except client)
+3.Emit (all client)
+*/
